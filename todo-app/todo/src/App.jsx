dd467d0a0fd22ee5f397e59613fe5e1411de0e0e
RehanAziz22 <rehanaziz284@gmail.com>
2022-09-26 21:44:57 +0500

    todo-app

--- todo-app/todo/src/App.jsx
+++ todo-app/todo/src/App.jsx
@@ -6,59 +6,80 @@ import Button from './components/Button';
 function App() {
 
   let [txt, setTxt] = useState("")
-  // let [input, setInput] = useState("")
   let [list, setList] = useState([])
+  let [editId, setEditId] = useState(0)
 
   const handelChange = (e) => {
     setTxt(e.target.value)
-    // console.log(txt)
-    // setList(list)
   }
 
-  function add(props) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
     if (!txt) {
       alert("txt is required")
       return;
     }
-    list.push(txt);
-    console.log(txt);
-    setList([{ id: `${txt}-${Date.now()}`, txt }, ...list]);
-    console.log(list);
-    setTxt('')
+    if (editId) {
+      const editTodo = list.find((i) => i.id === editId);
+      const updatedTodos = list.map((e) =>
+        e.id === editTodo.id
+          ? (e = { id: e.id, txt })
+          : { id: e.id, txt: e.txt }
+      );
+      setList(updatedTodos);
+      setEditId(0);
+      setTxt("");
+      return;
+    }
 
+    else if (txt !== "") {
+      setList([{ id: `${txt}-${Date.now()}`, txt }, ...list]);
+      console.log(txt)
+      console.log(list)
+      setTxt("");
+    }
   }
 
-  function edit() {
-
+  const handelDel = (id) => {
+    const delTodo = list.filter((i) => i.id !== id);
+    setList([...delTodo]);
   }
-  function del() {
-    // list.filter((e,i)=>{})
+
+  const handelEdit = (id) => {
+    const editTodo = list.find((i) => i.id === id);
+    setTxt(editTodo.txt);
+    setEditId(id);
+  }
+
+  function DelAll(){
+    setList([]);
 
   }
   return (
     <div className="App">
       <header className="App-header">
-        <h1 style={{
-          marginTop: 60
-        }}
-        >TODO APP</h1>
+        <div className='sectionOne'>
+          <h1 >TODO APP</h1>
 
-        <div className='container-One'>
-          <input
-            value={txt}
-            onChange={handelChange} />
-          <Button otherClass="btn" onClick={add} btnName="ADD" />
+          <div className='container-One'>
+            <input
+              value={txt}
+              onChange={handelChange} />
+            <Button otherClass="btn add" btnType="submit" onClick={handleSubmit} btnName={editId ? "EDIT" : "ADD"} />
+          </div>
+
+          <ul className='todoList'>
+            {list.map((e) => {
+              return <div className='display-con'>
+                <li className='listItems' ><span key={e.id}>{e.txt}</span></li>
+                <Button otherClass="btn del" onClick={() => handelEdit(e.id)} btnName="Edit" />
+                <Button otherClass="btn edit" onClick={() => handelDel(e.id)} btnName="Delete" />
+              </div>
+            })}
+          </ul>
+          <Button otherClass="btn delAll" onClick={DelAll} btnName="Delete All" />
         </div>
-
-        <ul className='list'>
-          {list.map((e,i) => {
-            return <div className='display-con'>
-              <li className='listItems' key={i}>{e}</li>
-              <Button otherClass="btn del" onClick={edit} btnName="Edit" />
-              <Button otherClass="btn" onClick={del} btnName="Delete" />
-            </div>
-          })}
-        </ul>
       </header>
     </div>
   );
