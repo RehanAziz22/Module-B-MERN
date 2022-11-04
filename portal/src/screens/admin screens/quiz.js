cd8d186123f1bef40cf5a6e75d5aaee2f4cfd7b3
RehanAziz22 <rehanaziz284@gmail.com>
2022-11-04 04:49:39 -0700

    quiz screen

--- portal/src/screens/admin screens/quiz.js
+++ portal/src/screens/admin screens/quiz.js
@@ -15,8 +15,10 @@ export default function Quiz() {
   let [module, setModule] = useState({});
   let [selectedCourse, setSelectedCourse] = useState("");
   let [btnDisabled, setBtnDisabled] = useState(false);
+  let [lockQuiz, setLockQuiz] = useState(true);
   const [question, setQuestion] = useState("")
   const [arrQuestions, setArrQuestions] = useState([])
+  const [allQuestions, setAllQuestions] = useState([])
   const [option, setOption] = useState("")
   const [arrOptions, setArrOptions] = useState([])
   const [correctAnswer, setCheckedOptions] = useState()
@@ -71,83 +73,95 @@ export default function Quiz() {
     arrQuestions[key] = val;
     setArrQuestions({ ...arrQuestions })
     console.log(arrQuestions)
+    
   }
-
+  
   //------------------
   const addOption = () => {
     // 👇️ push to end of state array
     setArrOptions([option, ...arrOptions]);
     fillQuestion("options", arrOptions)
   };
-
+  
   // set Correct option
   // const handleCheckBox = (e) => {
-  //   setCheckedOptions(e.target.checked)
-  //   // fillModule({ ...correctAnswer, correct: e.target.checked })
-  //   console.log(correctAnswer)
-  // }
-
-  const submitQuestion = () => {
-    fillModule("zuestion", arrQuestions)
-
+    //   setCheckedOptions(e.target.checked)
+    //   // fillModule({ ...correctAnswer, correct: e.target.checked })
+    //   console.log(correctAnswer)
+    // }
+    
+    const submitQuestion = () => {
+      setAllQuestions([...allQuestions,arrQuestions]);
+      console.log(allQuestions)
+      fillModule("allquestion",allQuestions)
+      console.log(allQuestions)
   }
+  
+  const lockQuizPage = () => {
+    // setLockQuiz(false)
+  }
+
   return (
     <>
       <Container sx={{ boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;", backgroundColor: "white", padding: "15px", borderRadius: "5px", width: { xs: "100%", md: "100%" } }}>
         <Grid container spacing={2}>
 
-          <Grid item xs={12} sm={12} md={12}>
+          {lockQuiz && <><Grid item xs={12} sm={12} md={12}>
             <Typography variant='h5' sx={{ fontWeight: "bold" }}>Create Quiz</Typography>
           </Grid>
 
-          {/* -----------------------------------------Section 1 */}
-          <Grid item xs={12} sm={12} md={12} sx={{ display: "flex" }}>
-            <MyTextField
-              // -------------------------------------> Quiz Name
-              label={"Quiz Name"}
-              required={true}
-              disabled={btnDisabled}
-              type={"text"}
-              onChange={(e) => { fillModule("quizName", e.target.value) }}
-            />
-            <MyTextField
-              // -------------------------------------> Course Name
-              label={"Quiz Score"}
-              required={true}
-              disabled={btnDisabled}
-              type={"text"}
-              onChange={(e) => { fillModule("quizScore", e.target.value) }}
-            />
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Course</InputLabel>
-              <Select
-                // -------------------------------------> Course 
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                // value={co}
-                label="Course"
-                defaultValue={""}
+            {/* -----------------------------------------Section 1 */}
+            <Grid item xs={12} sm={6} md={4} >
+              <MyTextField
+                // -------------------------------------> Quiz Name
+                label={"Quiz Name"}
+                required={true}
                 disabled={btnDisabled}
-                onChange={(e) => { fillModule("course", e.target.value) }}
-              >
-                {course.map((x, i) => {
-                  return <MenuItem key={i} value={x.fullName}>{x.fullName}</MenuItem>
+                type={"text"}
+                onChange={(e) => { fillModule("quizName", e.target.value) }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} >
 
-                })}
-              </Select>
-            </FormControl>
-            <Grid item xs={12} sm={12} md={12}>
+              <MyTextField
+                // -------------------------------------> Course Name
+                label={"Quiz Score"}
+                required={true}
+                disabled={btnDisabled}
+                type={"text"}
+                onChange={(e) => { fillModule("quizScore", e.target.value) }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={3} >
+
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Course</InputLabel>
+                <Select
+                  // -------------------------------------> Course 
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  // value={co}
+                  label="Course"
+                  defaultValue={""}
+                  disabled={btnDisabled}
+                  onChange={(e) => { fillModule("course", e.target.value) }}
+                >
+                  {course.map((x, i) => {
+                    return <MenuItem key={i} value={x.fullName}>{x.fullName}</MenuItem>
+
+                  })}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6} md={1} sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
               <Button variant="contained"
                 sx={{ backgroundImage: "linear-gradient( 109.6deg, rgb(107 155 227) 11.2%, rgba(110,123,251,1) 91.1% );" }} disabled={btnDisabled} onClick={nextSection}>Next</Button>
             </Grid>
-          </Grid>
 
-          <Divider />
-          {/*---------------------------------- Section 2 */}
-          {btnDisabled &&
-
-            <>
-              <Grid item xs={12} sm={12} md={12} sx={{ display: "flex" }}>
+            <Divider />
+            {/*---------------------------------- Section 2 */}
+            {btnDisabled && <>
+              <Grid item xs={12} sm={12} md={11} >
                 <MyTextField
                   // -------------------------------------> Question
                   label={"Question"}
@@ -158,8 +172,8 @@ export default function Quiz() {
                   }}
                 />
               </Grid>
-              <Grid item xs={12} sm={12} md={12} sx={{ display: "flex" }}>
-              {/*------------------------------------------- check Box  */}
+              <Grid item xs={12} sm={12} md={12} >
+                {/*------------------------------------------- check Box  */}
                 {arrOptions.length > 0 ? <Box sx={{ display: 'flex' }}>
                   <FormControl sx={{ m: 3, textAlign: "left" }} component="fieldset" variant="standard" >
                     <FormLabel component="legend">Options</FormLabel>
@@ -180,7 +194,7 @@ export default function Quiz() {
                   </FormControl>
                 </Box> : null}
               </Grid>
-              <Grid item xs={12} sm={12} md={12} sx={{ display: "flex" }}>
+              <Grid item xs={12} sm={8} md={9} >
 
                 <MyTextField
                   // -------------------------------------> options
@@ -189,20 +203,52 @@ export default function Quiz() {
                   type={"text"}
                   onChange={(e) => { setOption(e.target.value) }}
                 />
-                <Button variant="contained" sx={{ backgroundImage: "linear-gradient( 109.6deg, rgb(107 155 227) 11.2%, rgba(110,123,251,1) 91.1% );" }} onClick={addOption}>Add Option</Button>
 
               </Grid>
-              <Grid item xs={12} sm={12} md={12}>
+              <Grid item xs={12} sm={4} md={2} sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <Button variant="contained" sx={{ backgroundImage: "linear-gradient( 109.6deg, rgb(107 155 227) 11.2%, rgba(110,123,251,1) 91.1% );" }} onClick={addOption}>Add Option</Button>
+              </Grid>
+
+              <Grid item xs={6} sm={6} md={6}>
                 <Button variant="contained" sx={{ backgroundImage: "linear-gradient( 109.6deg, rgb(107 155 227) 11.2%, rgba(110,123,251,1) 91.1% );" }} onClick={submitQuestion}>Add Question</Button>
               </Grid>
-              <Grid item xs={12} sm={12} md={12}>
-                <Button variant="contained" sx={{ backgroundImage: "linear-gradient( 109.6deg, rgb(107 155 227) 11.2%, rgba(110,123,251,1) 91.1% );" }} onClick={sendQuizData}>Submit Form</Button>
+              <Grid item xs={6} sm={6} md={6}>
+                <Button variant="contained" sx={{ backgroundImage: "linear-gradient( 109.6deg, rgb(107 155 227) 11.2%, rgba(110,123,251,1) 91.1% );" }} onClick={lockQuizPage}>lock Quiz</Button>
               </Grid>
+            </>}</>}
 
-            </>
+          {lockQuiz && <>
+          <Grid item xs={12} sm={12} md={12}>
+            <Button variant="contained" sx={{ backgroundImage: "linear-gradient( 109.6deg, rgb(107 155 227) 11.2%, rgba(110,123,251,1) 91.1% );" }} onClick={sendQuizData}>Submit Form</Button>
+          </Grid>
+          {/* <Grid item xs={12} sm={12} md={12}>
+          {question.map((e,i)=>{
+            const{quizName,quizScore,course} = e
+            return <Grid container spacing={2}>
+          <Grid item xs={12} sm={12} md={12}>
+          <Typography variant='h5' sx={{ fontWeight: "bold" }}>{quizName}</Typography>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12}>
+          <Typography variant='h5' sx={{ fontWeight: "bold" }}>{quizScore}</Typography>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12}>
+          <Typography variant='h5' sx={{ fontWeight: "bold" }}>{course}</Typography>
+          </Grid>
+          
+          <Grid item xs={12} sm={12} md={12}>
+          <Typography variant='h5' sx={{ fontWeight: "bold" }}>{e.question.question}</Typography>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12}>
+            <ul>
+              <li>{Object.values(e.arrOptions)}</li>
+            </ul>
+          </Grid>
+            </Grid>
+          })}
+          </Grid> */}
 
+          </>
           }
-
         </Grid>
       </Container></>
   )
